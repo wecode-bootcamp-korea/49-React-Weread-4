7c328328b5d260e3a711ab70035efad71d707073
jyj <dudwns5013@naver.com>
2023-09-01 16:01:16 +0900

    세팅수정 2

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/pages/Login/Login";
+import Nav from "./components/Nav/Nav";
+import Main from "./components/pages/Main/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/nav" element={<Nav />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
