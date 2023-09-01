2f7ef8462b3e9b040f6be0c986c7380e474c7a7f
sylee <soyeun.lee5930@gmail.com>
2023-09-01 18:12:36 +0900

    [49기-이소연]WEREAD 소스코드

---
+++ src/components/pages/JoinInfo/JoinInfo.js
@@ -0,0 +1,128 @@
+import React from "react";
+import "./JoinInfo.scss";
+import { useNavigate } from "react-router-dom";
+
+const Main = () => {
+  const navigate = useNavigate();
+
+  const SelectsBirthYear = () => {
+    const years = [];
+    for (let i = 2023; i >= 1920; i--) {
+      years.push(<option key={i}>{i}년</option>);
+    }
+    return (
+      <select className="birthYear" id="birthSelect" defaultValue="2000년">
+        {years}
+      </select>
+    );
+  };
+
+  const SelectsBirtMonth = () => {
+    const months = [];
+    for (let i = 1; i <= 12; i++) {
+      months.push(<option key={i}>{i}월</option>);
+    }
+    return (
+      <select className="birthMonth" defaultValue="1월">
+        {months}
+      </select>
+    );
+  };
+
+  const SelectsBirtDay = () => {
+    const days = [];
+    for (let i = 1; i <= 31; i++) {
+      days.push(<option key={i}>{i}일</option>);
+    }
+    return (
+      <select className="birthDay" defaultValue="1일">
+        {days}
+      </select>
+    );
+  };
+
+  return (
+    <div className="main">
+      <div className="joinInfo">
+        <header>
+          <div className="backDiv">
+            <img
+              className="backArrow"
+              src="/images/Back_arrow.jpg"
+              alt="뒤로가기"
+              onClick={() => navigate(-1)}
+            ></img>
+            <span className="back">뒤로</span>
+          </div>
+        </header>
+        <section>
+          <h1>회원가입</h1>
+          <div className="text">
+            <p className="text1">기본 정보</p>
+            <p className="text2">필수 사항</p>
+          </div>
+          <div className="list">
+            <input className="email" type="email" placeholder="이메일"></input>
+            <input
+              className="pwd"
+              type="password"
+              placeholder="비밀번호"
+            ></input>
+            <input
+              className="pwdCheck"
+              type="password"
+              placeholder="비밀번호확인"
+            ></input>
+          </div>
+          <div className="nick">
+            <label htmlFor="nickName">
+              닉네임과 프로필 이미지
+              <span className="notEssential">선택 사항</span>
+            </label>
+            <input id="nickName" type="text" placeholder="닉네임"></input>
+          </div>
+          <div className="phone">
+            <label htmlFor="phoneNumber">
+              전화번호<span className="notEssential">선택 사항</span>
+            </label>
+            <div className="selectInput">
+              <select defaultValue="010">
+                <option value="010">010</option>
+                <option value="011">011</option>
+                <option value="016">016</option>
+                <option value="017">017</option>
+                <option value="019">019</option>
+              </select>
+              <input
+                id="phoneNumber"
+                type="tel"
+                placeholder="휴대폰 번호를 입력하세요."
+              ></input>
+            </div>
+          </div>
+          <div className="birth">
+            <label htmlFor="birthSelect">
+              생일<span className="notEssential">선택 사항</span>
+            </label>
+            <div className="selectBox">
+              <div className="yearWrap">
+                <SelectsBirthYear />
+              </div>
+              <div className="monthWrap">
+                <SelectsBirtMonth />
+              </div>
+              <div className="dayWrap">
+                <SelectsBirtDay />
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer>
+          <button>회원 가입</button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,7 +1,46 @@
+import React from "react";
 import "./Login.scss";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>안녕하세요</div>;
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="imageContainer">
+          <img
+            className="wecodeLogo"
+            src="/images/Logo.png"
+            alt="위코드로고"
+          ></img>
+          <img
+            className="logoText"
+            src="/images/logo_wecode.png"
+            alt="로고글씨"
+          ></img>
+        </div>
+        <div className="inputContainer">
+          <input className="email" type="email" placeholder="이메일"></input>
+          <input
+            className="password"
+            type="password"
+            placeholder="비밀번호"
+          ></input>
+          <button className="loginButton">로그인</button>
+          <div className="loginOption">
+            <div className="join">
+              <Link to="/joinInfo">
+                <p>회원 가입</p>
+              </Link>
+            </div>
+            <div className="divide">|</div>
+            <div className="passwordFind">
+              <p>비밀번호 찾기</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
