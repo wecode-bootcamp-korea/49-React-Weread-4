0b3ab783733941493d55b592ade20f434864f3fb
이영은 <hisenselee12@gmail.com>
2023-09-04 13:23:28 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React ,{ useState } from "react";
 import "./Login.scss";
 import {Link} from "react-router-dom";
 
 const Login = () => {
 
+
+  const [userEmail, setUserEmail] = useState('');
+  const saveUserId = (event)=> {
+    setUserEmail(event.target.value);
+    console.log(event.target.value)
+  }
+
+  const [userPassword, setUserPassword] = useState('')
+  const saveUserPassword = (event)=> {
+    setUserPassword(event.target.value);
+    console.log(event.target.value)
+  }
 
   return (
     <div className="login">
@@ -12,8 +24,14 @@ const Login = () => {
         <img className="textLogo" src={process.env.PUBLIC_URL + '/logo_wecode.png'} alt="위코드의 한글 로고" />
       </header>
       <section className="textBlock">
-        <input type="email" id="email" placeholder="이메일"/>
-        <input type="password" id="password" placeholder="비밀번호"/>
+        <input type="email" id="email" placeholder="이메일"
+        value={userEmail}
+        onChange={saveUserId}
+        />
+        <input type="password" id="password" placeholder="비밀번호"
+        value={userPassword}
+        onChange={saveUserPassword}
+        />
         <input type="button" id="btn" value="로그인"/>
         <ul>
           <li><Link to='/main'>회원 가입</Link></li>
