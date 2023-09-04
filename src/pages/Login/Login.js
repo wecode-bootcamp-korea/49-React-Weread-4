c4cf2353920523130677735f5519b2a781c7d9ce
이영은 <hisenselee12@gmail.com>
2023-09-04 18:54:04 +0900

    Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,51 @@
 import React ,{ useState } from "react";
 import "./Login.scss";
-import {Link} from "react-router-dom";
+import { Link, } from "react-router-dom";
 
 const Login = () => {
 
 
+  const [isTurnOn, setIsTurnOn] = useState(false);
+  const [className, setClassName] = useState("btn");
+  const [userPassword, setUserPassword] = useState('')
   const [userEmail, setUserEmail] = useState('');
+
+
   const saveUserId = (event)=> {
     setUserEmail(event.target.value);
-    console.log(event.target.value)
+
+    userEmail.includes('@') && userPassword.length>=5
+    ? setIsTurnOn(true)
+    : setIsTurnOn(false);
+
+    userEmail.includes('@') && userPassword.length>=5
+    ? setClassName('btn')
+    : setClassName('btnOn');
+    
   }
 
-  const [userPassword, setUserPassword] = useState('')
+
   const saveUserPassword = (event)=> {
     setUserPassword(event.target.value);
-    console.log(event.target.value)
+
+    userEmail.includes('@') && userPassword.length>=5
+    ? setIsTurnOn(true)
+    : setIsTurnOn(false);
+
+    userEmail.includes('@') && userPassword.length>=5
+    ? setClassName('btn')
+    : setClassName('btnOn');
+
   }
+
+  // const bgChange=()=>{
+  //   setClassName("btn");
+
+  //   return !setIsTurnOn(false)
+  //   ? className("btnOn")
+  //   : className("btn");
+
+  // }
 
   return (
     <div className="login">
@@ -32,7 +62,11 @@ const Login = () => {
         value={userPassword}
         onChange={saveUserPassword}
         />
-        <input type="button" id="btn" value="로그인"/>
+        <div className="btnWrap">
+          <input type="button"
+          className={className}
+          disabled={isTurnOn} value="로그인" />
+        </div>
         <ul>
           <li><Link to='/main'>회원 가입</Link></li>
           <li>비밀번호 찾기</li>
